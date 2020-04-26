ce81944490c081b63dfac81dcd4d85b88f726d7b
xavi-develop <xavi.creus@outlook.com>
2020-04-26 18:27:21 +0200

    Versió 0.2 del projecte prjava01

--- src/prjava21/prjava21.java
+++ src/prjava21/prjava21.java
@@ -12,6 +12,7 @@ public class prjava21 {
     public static void main(String[] args) throws IOException {
         File f = new File("fitxer.html");
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write(" <h1>DAW2 m08uf4pr2</h1>");bw.newLine(); 
             bw.write("<html>");bw.newLine();
             bw.write("  <head>");bw.newLine();
             bw.write("    <title>");bw.newLine();
